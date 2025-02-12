bbc75dadaa6d443024c91d3ecde62726328c5a49
Blacky Cat <paule32.jk@gmail.com>
2025-02-12 19:52:37 +0100

    Create my_dll.cpp

---
+++ test/my_dll.cpp
@@ -0,0 +1,20 @@
+#include <windows.h>
+
+#define EXPORT __declspec(dllexport)
+
+extern "C" {
+    EXPORT int add(int a, int b) {
+        return a + b;
+    }
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+        case DLL_PROCESS_ATTACH:
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
